1cb60e805283764a0035528f26d1103ce446979c
ZeroDayTea <patrick.dobranowski@gmail.com>
2026-04-17 15:29:38 -0500

    updated sponsors and dockerignore

--- app/(home)/Sponsors.tsx
+++ app/(home)/Sponsors.tsx
@@ -17,6 +17,34 @@ export default function Sponsors() {
 		Veria autonomously maps, exploits, and secures your entire attack surface.
             </Sponsor>
 
+            <Sponsor
+                href="https://www.paloaltonetworks.com/"
+                src="/assets/sponsors/paloaltonetworks.jpg"
+                name="Palo Alto Networks"
+            >
+            </Sponsor>
+
+            <Sponsor
+                href="https://zellic.io"
+                src="/assets/sponsors/zellic_logos_primary/RGB_zellic-logo-primary-dark.png"
+                name="Zellic"
+            >
+                Zellic is a security research firm. Our targets include compilers, virtual machines, web apps, circuits, proof systems, and more. Before Zellic, we previously founded perfect blue, the #1 CTF team in 2020 and 2021. If you're smart and good at CTFs, we'd love to meet you.
+                <br /><br />
+                We offer a complete benefits package and direct equity participation. We also offer flexible hours, remote work, and both full-time and part-time roles. Our team enjoys regular fully-funded offsites and a range of other perks.
+                <br /><br />
+                Ask your friends: you might already know someone who works here.
+                <br /><br />
+                To learn more, check out our blog: <a href="https://zellic.io/auditooor-grindset" target="_blank" rel="noopener noreferrer" className="text-theme-bright hover:underline">zellic.io/auditooor-grindset</a>
+                <br /><br />
+                Work at Zellic: <a href="mailto:jobs@zellic.io" className="text-theme-bright hover:underline">jobs@zellic.io</a> | <a href="https://zellic.io/careers" target="_blank" rel="noopener noreferrer" className="text-theme-bright hover:underline">zellic.io/careers</a> | @gf_256
+                <br /><br />
+                Meet the founders:
+                <br />
+                - Luna: @gf_256 on Discord (<a href="https://x.com/gf_256" target="_blank" rel="noopener noreferrer" className="text-theme-bright hover:underline">Twitter</a>)
+                <br />
+                - Jazzy @jazzyzellic on Discord (<a href="https://x.com/ret2jazzy" target="_blank" rel="noopener noreferrer" className="text-theme-bright hover:underline">Twitter</a>)
+            </Sponsor>
 
             {/* <Sponsor
                 href="https://www.trailofbits.com/"
